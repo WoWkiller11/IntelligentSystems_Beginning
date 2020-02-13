25c0b5a94360d4a463451aee6f4d6fdc4f7002f3
Ринат <renich66666622@gmail.com>
2020-02-13 17:45:11 +0300

    ll

--- lab1/network/Program.cs
+++ lab1/network/Program.cs
@@ -8,7 +8,7 @@ namespace lab1.network
     {
         static void Main(string[] args)
         {
-            Perceptron p = new Perceptron(3, 15, 2);
+            Perceptron p = new Perceptron(3, 17, 2);
             Random rnd = new Random(DateTime.Now.Millisecond);
             double[][] red_set;
             double[][] blue_set;
@@ -48,7 +48,7 @@ namespace lab1.network
             int choice, elem;
 
 
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 110000; i++)
             {
                 choice = rnd.Next(3);
 
@@ -64,10 +64,13 @@ namespace lab1.network
                 }
             }
 
-
+            double[] test = new double[] { 1.0d, 0.21111d, 0.6112d };
             double[] ans = p.CountResult(ref red_set[33]);
             Console.WriteLine(ans[0] + "    " + ans[1]);
             ans = p.CountResult(ref red_set[28]);
+            Console.WriteLine(ans[0] + "    " + ans[1]);
+
+            ans = p.CountResult(ref test);
             Console.WriteLine(ans[0] + "    " + ans[1]);
         }
     }
